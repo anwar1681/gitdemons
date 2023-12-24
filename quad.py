7d93b73b93e16ed406c5401353a1cdc69a29e1a5
=Anwar <anwar1681@yahoo.com>
2023-12-24 12:44:57 +0500

    modify file

--- quad.py
+++ quad.py
@@ -1,11 +1,17 @@
 
 print("ax^2 +bx + c = 0")
+def quad():
+   
+    a = int(input('enter value of a: '))
+    b = int(input('enter value of b: '))
+    c = int(input('enter value of c: '))
 
-a = int(input('enter value of a: '))
-b = int(input('enter value of b: '))
-c = int(input('enter value of c: '))
+    D = round((b**2 - 4*a*c)**0.5,2)
+    x1 = round((-b + D)/(2*a),2)
+    x2 = round((-b - D)/(2*a),2)
 
-D = round((b**2 - 4*a*c)**0.5,2)
-x1 = (-b + D)/(2*a)
-x2 = (-b - D)/(2*a)
+    print('First root x1: ',x1)
+    print('Second root x2: ',x2)
 
+# if __name__=="__main__":
+quad() 
